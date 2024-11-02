210c1e132db17a90780e690496ef09f266867cc7
Marco van Oort <marcovoort@gmail.com>
2024-11-02 17:21:57 +0100

    Add run5

---
+++ run5.sh
@@ -0,0 +1,104 @@
+set -e
+
+# All datasets, only artificial noise
+
+### Dataset 0
+
+# Artificial noise
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 .5 10 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 .25 10 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 .125 10 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 .0625 10 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 .03125 10 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 2 10 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 4 10 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 8 10 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 16 10 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 32 10 0
+
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 1 5 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 1 2.5 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 1 1.25 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 1 0.625 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 1 0.3125 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 1 20 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 1 40 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 1 80 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 1 160 0
+python.exe ./SLAM.py run_results/run5/ 0 100 40 1 1 320 0
+
+
+### Dataset 1
+
+# Artificial noise
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 .5 10 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 .25 10 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 .125 10 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 .0625 10 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 .03125 10 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 2 10 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 4 10 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 8 10 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 16 10 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 32 10 0
+
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 1 5 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 1 2.5 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 1 1.25 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 1 0.625 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 1 0.3125 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 1 20 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 1 40 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 1 80 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 1 160 0
+python.exe ./SLAM.py run_results/run5/ 1 100 40 1 1 320 0
+
+### Dataset 2
+
+# Artificial noise
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 .5 10 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 .25 10 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 .125 10 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 .0625 10 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 .03125 10 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 2 10 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 4 10 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 8 10 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 16 10 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 32 10 0
+
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 1 5 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 1 2.5 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 1 1.25 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 1 0.625 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 1 0.3125 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 1 20 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 1 40 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 1 80 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 1 160 0
+python.exe ./SLAM.py run_results/run5/ 2 100 40 1 1 320 0
+
+### Dataset 3
+
+# Artificial noise
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 .5 10 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 .25 10 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 .125 10 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 .0625 10 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 .03125 10 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 2 10 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 4 10 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 8 10 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 16 10 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 32 10 0
+
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 1 5 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 1 2.5 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 1 1.25 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 1 0.625 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 1 0.3125 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 1 20 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 1 40 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 1 80 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 1 160 0
+python.exe ./SLAM.py run_results/run5/ 3 100 40 1 1 320 0
