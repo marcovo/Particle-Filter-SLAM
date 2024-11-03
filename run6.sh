8b2c08a3db3fff2f3c40f3bd72faf9c62a2a2c02
Marco van Oort <marcovoort@gmail.com>
2024-11-03 10:29:21 +0100

    Add run6

---
+++ run6.sh
@@ -0,0 +1,59 @@
+set -e
+
+# All datasets, only artificial noise (translation + rotation combined)
+
+### Dataset 0
+
+# Artificial noise
+python.exe ./SLAM.py run_results/run6/ 0 100 40 1 .5 5 0
+python.exe ./SLAM.py run_results/run6/ 0 100 40 1 .25 2.5 0
+python.exe ./SLAM.py run_results/run6/ 0 100 40 1 .125 1.25 0
+python.exe ./SLAM.py run_results/run6/ 0 100 40 1 .0625 0.625 0
+python.exe ./SLAM.py run_results/run6/ 0 100 40 1 .03125 0.3125 0
+python.exe ./SLAM.py run_results/run6/ 0 100 40 1 2 20 0
+python.exe ./SLAM.py run_results/run6/ 0 100 40 1 4 40 0
+python.exe ./SLAM.py run_results/run6/ 0 100 40 1 8 80 0
+python.exe ./SLAM.py run_results/run6/ 0 100 40 1 16 160 0
+python.exe ./SLAM.py run_results/run6/ 0 100 40 1 32 320 0
+
+### Dataset 1
+
+# Artificial noise
+python.exe ./SLAM.py run_results/run6/ 1 100 40 1 .5 5 0
+python.exe ./SLAM.py run_results/run6/ 1 100 40 1 .25 2.5 0
+python.exe ./SLAM.py run_results/run6/ 1 100 40 1 .125 1.25 0
+python.exe ./SLAM.py run_results/run6/ 1 100 40 1 .0625 0.625 0
+python.exe ./SLAM.py run_results/run6/ 1 100 40 1 .03125 0.3125 0
+python.exe ./SLAM.py run_results/run6/ 1 100 40 1 2 20 0
+python.exe ./SLAM.py run_results/run6/ 1 100 40 1 4 40 0
+python.exe ./SLAM.py run_results/run6/ 1 100 40 1 8 80 0
+python.exe ./SLAM.py run_results/run6/ 1 100 40 1 16 160 0
+python.exe ./SLAM.py run_results/run6/ 1 100 40 1 32 320 0
+
+### Dataset 2
+
+# Artificial noise
+python.exe ./SLAM.py run_results/run6/ 2 100 40 1 .5 5 0
+python.exe ./SLAM.py run_results/run6/ 2 100 40 1 .25 2.5 0
+python.exe ./SLAM.py run_results/run6/ 2 100 40 1 .125 1.25 0
+python.exe ./SLAM.py run_results/run6/ 2 100 40 1 .0625 0.625 0
+python.exe ./SLAM.py run_results/run6/ 2 100 40 1 .03125 0.3125 0
+python.exe ./SLAM.py run_results/run6/ 2 100 40 1 2 20 0
+python.exe ./SLAM.py run_results/run6/ 2 100 40 1 4 40 0
+python.exe ./SLAM.py run_results/run6/ 2 100 40 1 8 80 0
+python.exe ./SLAM.py run_results/run6/ 2 100 40 1 16 160 0
+python.exe ./SLAM.py run_results/run6/ 2 100 40 1 32 320 0
+
+### Dataset 3
+
+# Artificial noise
+python.exe ./SLAM.py run_results/run6/ 3 100 40 1 .5 5 0
+python.exe ./SLAM.py run_results/run6/ 3 100 40 1 .25 2.5 0
+python.exe ./SLAM.py run_results/run6/ 3 100 40 1 .125 1.25 0
+python.exe ./SLAM.py run_results/run6/ 3 100 40 1 .0625 0.625 0
+python.exe ./SLAM.py run_results/run6/ 3 100 40 1 .03125 0.3125 0
+python.exe ./SLAM.py run_results/run6/ 3 100 40 1 2 20 0
+python.exe ./SLAM.py run_results/run6/ 3 100 40 1 4 40 0
+python.exe ./SLAM.py run_results/run6/ 3 100 40 1 8 80 0
+python.exe ./SLAM.py run_results/run6/ 3 100 40 1 16 160 0
+python.exe ./SLAM.py run_results/run6/ 3 100 40 1 32 320 0
